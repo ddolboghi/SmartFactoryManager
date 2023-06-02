705a62ad6ea514c334908d3ee52e24d362a99b12
DESKTOP-KG50QAO\Administrator <kwonoj999@gmail.com>
2023-06-02 17:42:42 +0900

    ojun update

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt # matplotlib
 import librosa
 
+import pickle
+from sklearn import metrics
 # 경고 방지
 import warnings # 워닝 방지
 warnings.filterwarnings('ignore')
@@ -38,58 +40,80 @@ def molding():
     return render_template('molding/index3.html')
 
 # 오준, 재윤
-@app.route('/drying')
+@app.route('/drying', methods=['GET', 'POST'])
 def drying():
-    lstm_ae = load_model('./data/lstm-ae1.h5')
-    err_Name='C:/final_prj/dataset_sound/data/FAN_sound_error'
-    ok_Name='C:/final_prj/dataset_sound/data/FAN_sound_OK'
+    if request.method == 'POST':
+        audio_files = request.files.getlist('audio')
+  
 
-    df=pd.DataFrame(columns=['mfcc_min','mfcc_max','spectrum_min','spectrum_max'])
-    df['NG']=0
-     
-    def mk_Frequency(y,sr):
-        fft=np.fft.fft(y)
-        magnitude=np.abs(fft)
-        fre=np.linspace(0,sr,len(magnitude))
-        haf_spectrum=magnitude[:int(len(magnitude)/2)]
-        haf_fre=fre[:int(len(magnitude)/2)]
-        return haf_spectrum, haf_fre
-    
-    spectrum_min=list() 
-    spectrum_max=list() 
-    mfcc_min=list() 
-    mfcc_max=list()
-    ng=list()
-    a=0 
-    b=0 
-    for n in range(183):
-        if (len(glob.glob(err_Name+'/*'))-1) >= n:
-            path=glob.glob(ok_Name+'/*')[a]
-            err,ser=librosa.load(path,sr=100)
-            left_spectrum,left_f_ng=mk_Frequency(err,ser)
-            mfcc=librosa.feature.mfcc(y=err,sr=ser)
-            ng.append(1)
-            a+=1
-        else:
-            path=glob.glob(ok_Name+'/*')[b]
-            okr,skr=librosa.load(path,sr=100)
-            left_spectrum,left_f_ok=mk_Frequency(okr,skr)
-            mfcc=librosa.feature.mfcc(y=okr,sr=skr)
-            ng.append(0)
-            b+=1
+        df = pd.DataFrame(columns=['mfcc_min', 'mfcc_max', 'spectrum_min', 'spectrum_max'])
+        df['NG'] = 0
+
+        spectrum_min = []
+        spectrum_max = []
+        mfcc_min = []
+        mfcc_max = []
+        ng = []
+
+        for audio_file in audio_files:
+            err, ser = librosa.load(audio_file, sr=100)
+            left_spectrum, left_f_ng = mk_Frequency(err, ser)
+            mfcc = librosa.feature.mfcc(y=err, sr=ser)
             
-        spectrum_min.append(min(left_spectrum))
-        spectrum_max.append(max(left_spectrum))
-        mfcc_min.append(mfcc.min())
-        mfcc_max.append(mfcc.max())
-    df['spectrum_min']=spectrum_min 
-    df['spectrum_max']=spectrum_max
-    df['mfcc_min']=mfcc_min 
-    df['mfcc_max']=mfcc_max
-    df['NG']=ng
-    
-    return render_template('sound/dataframe.html', table=df.to_html())
+            if 'error' in audio_file.filename:
+                ng.append(1)
+            else:
+                ng.append(0)
 
+            spectrum_min.append(min(left_spectrum))
+            spectrum_max.append(max(left_spectrum))
+            mfcc_min.append(mfcc.min())
+            mfcc_max.append(mfcc.max())
+
+        df['spectrum_min'] = spectrum_min
+        df['spectrum_max'] = spectrum_max
+        df['mfcc_min'] = mfcc_min
+        df['mfcc_max'] = mfcc_max
+        df['NG'] = ng
+
+        data = df.iloc[:, :-2]
+        # target = df.iloc[:, -1:]
+        
+        ments = []
+        
+        if mfcc.min() < -411 :
+            ments.append("mfcc_min")
+        elif ((mfcc.max() > 33) and mfcc.max() < 20 ):
+            ments.append("mfcc_max")            
+            
+        with open('C:\\SmartFactoryManager\\model.dtc', 'rb') as file:
+            loaded_model = pickle.load(file)
+
+        pred_y = loaded_model.predict(data)
+       
+        
+        return render_template('/sound/sound.html', pred_y = pred_y, ments = ments)
+
+
+    return render_template('/sound/upload.html')
+
+
+def mk_Frequency(y, sr):
+    fft = np.fft.fft(y)
+    magnitude = np.abs(fft)
+    fre = np.linspace(0, sr, len(magnitude))
+    haf_spectrum = magnitude[:int(len(magnitude)/2)]
+    haf_fre = fre[:int(len(magnitude)/2)]
+    return haf_spectrum, haf_fre
+
+
+
+
+
+
+
+    
+    
 @app.route('/ques_concern', methods=['GET', 'POST'])
 def question_type():        
     return render_template('index3.html')
