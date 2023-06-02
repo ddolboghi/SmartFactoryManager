c06e14b66702e52830be5f7e4403acefd92df4a7
[ddolboghi] <[rhdfyd128@gmail.com]>
2023-06-02 15:08:59 +0900

    main page update

---
+++ static/js/index.js
@@ -0,0 +1,7 @@
+function showText(id) {
+    document.getElementById(`text${id}`).style.opacity = "1";
+}
+  
+function hideText(id) {
+    document.getElementById(`text${id}`).style.opacity = "0";
+}
