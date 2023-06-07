f87b1cdc4517e17cbea142f580358f08fc1bfaf0
[ddolboghi] <[rhdfyd128@gmail.com]>
2023-06-08 08:37:22 +0900

    mainpage update

--- static/js/index.js
+++
@@ -1,7 +0,0 @@
-function showText(id) {
-    document.getElementById(`text${id}`).style.opacity = "1";
-}
-  
-function hideText(id) {
-    document.getElementById(`text${id}`).style.opacity = "0";
-}
